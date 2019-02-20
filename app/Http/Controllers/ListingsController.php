06b923b31a37f676ce47fb350631785b4143304d
gphinith <gary@taylorprops.com>
2019-02-20 16:31:44 -0500

    changing controller name

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Listings;
 use Illuminate\Http\Request;
 
-class Listings extends Controller
+class ListingsController extends Controller
 {
 
     public function featuredListings()
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', 'Listings@featuredListings');
+Route::get('/', 'ListingsController@featuredListings');
 
